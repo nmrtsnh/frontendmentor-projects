5c6684820b88fe617d518ebbe7ed9def336218a8
nmrtsnh <namratasinha26@gmail.com>
2023-04-06 01:35:17 +0200

    update

--- social-media-dashboard-with-theme-switcher-master/script.js
+++ social-media-dashboard-with-theme-switcher-master/script.js
@@ -3,7 +3,7 @@ const topBg = document.querySelector("top-bg");
 
 //Switch Theme
 function switchTheme(event) {
-  // console.log(event.target.checked);
+  console.log(event.target.checked);
 
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "light");
