6ba5d2a48b516c8d3a9ffeee86663432fbe8176f
nmrtsnh <namratasinha26@gmail.com>
2023-12-08 00:55:35 +0100

    update functionality

--- ping-coming-soon-page/script.js
+++ ping-coming-soon-page/script.js
@@ -1,28 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Wait for the DOM content to be loaded before adding event listeners
+const emailInput = document.getElementById("email");
+const notifyBtn = document.querySelector(".notify-btn");
+const errorMsg = document.getElementById("errorMsg");
 
-  // Get the necessary elements
-  const emailInput = document.getElementById("email");
-  const notifyBtn = document.querySelector(".notify-btn");
-  const errorMsg = document.getElementById("errorMsg");
+notifyBtn.addEventListener("click", function (e) {
+  e.preventDefault();
 
-  // Add click event listener to the "Notify me" button
-  notifyBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    // Log the value to see what's being captured
-    console.log("Email input value:", emailInput.value);
-
-    // Trim the value and check if it's empty
-    if (
-      emailInput.value.trim() === "" ||
-      emailInput.value === "Your email address..."
-    ) {
-      console.log("Email is empty");
-      errorMsg.style.display = "block";
-    } else {
-      console.log("Email is not empty");
-      errorMsg.style.display = "none";
-    }
-  });
+  if (
+    emailInput.value.trim() === "" ||
+    emailInput.value === "Your email address..."
+  ) {
+    errorMsg.style.display = "block";
+  } else {
+    errorMsg.style.display = "none";
+  }
 });
