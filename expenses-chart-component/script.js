fe4f806e631b7bb899dedc6b4703a82b8609627f
nmrtsnh <namratasinha26@gmail.com>
2023-12-11 14:27:08 +0100

    update javasript

--- expenses-chart-component/script.js
+++ expenses-chart-component/script.js
@@ -1,23 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Get all spending bar elements
-  const spendingBars = document.querySelectorAll(".spending-bars");
+const spendingBars = document.querySelectorAll(".spending-bars");
 
-  // Add event listeners to each spending bar
-  spendingBars.forEach((bar, index) => {
-    bar.addEventListener("mouseover", () => {
-      // Get the corresponding spending amount element
-      const spendingAmount = bar.querySelector(".spending-amount");
+spendingBars.forEach((bar, index) => {
+  bar.addEventListener("mouseover", () => {
+    const spendingAmount = bar.querySelector(".spending-amount");
 
-      // Make the spending amount visible
-      spendingAmount.style.visibility = "visible";
-    });
+    spendingAmount.style.visibility = "visible";
+  });
 
-    bar.addEventListener("mouseout", () => {
-      // Get the corresponding spending amount element
-      const spendingAmount = bar.querySelector(".spending-amount");
+  bar.addEventListener("mouseout", () => {
+    const spendingAmount = bar.querySelector(".spending-amount");
 
-      // Hide the spending amount
-      spendingAmount.style.visibility = "hidden";
-    });
+    spendingAmount.style.visibility = "hidden";
   });
 });
